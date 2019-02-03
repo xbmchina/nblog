01579e29a08ba47b48abeb4ae650556225fa3b82
xbmchina <1131663064@163.com>
2019-02-03 15:21:54 +0800

    第一次提交

---
+++ src/main/java/cn/xbmchina/nblog/NblogApplication.java
@@ -0,0 +1,14 @@
+package cn.xbmchina.nblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NblogApplication.class, args);
+	}
+
+}
+
